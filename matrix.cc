60746159ea60482e73aabcccc1b32c6840dec92d
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-09-15 01:33:45 -0500

    print matrix

--- matrix.cc
+++ matrix.cc
@@ -16,21 +16,14 @@ Vector getCol(const Matrix &m, int numCol) {
   return col;
 }
 
-void diamondCol(const Matrix &m1, const Matrix &m2, int nCol, Matrix &result,
-                int index, vector<bool> &available, int &cont) {
-
-  available[index] = true;
-  vector<int> col = getCol(m2, nCol);
-  for (int i = 0; i < m1.size(); i++) {
-    int mn = numeric_limits<int>::max();
-    for (int j = 0; j < m1[i].size(); j++) {
-      mn = min(mn, m1[i][j] + col[j]);
+void print(Matrix &m) {
+  for (int i = 0; i < m.size(); i++) {
+    for (int j = 0; j < m[i].size(); j++) {
+      if (j) cout << " ";
+      cout << m[i][j];
     }
-    result[i][nCol] = mn;
+    cout << endl;
   }
-
-  cont++;
-  available[index] = false;
 }
 
 void multCol(const Matrix &m1, const Matrix &m2, int nCol, Matrix &result,
@@ -55,9 +48,10 @@ void multConcurrency(const Matrix &m1, const Matrix &m2) {
   queue<int> q;
   Matrix result(m1.size(), Vector(m2[0].size(), 0));
   int n = thread::hardware_concurrency();
-  cout << "nucleos: " << n << endl;
+
   vector<bool> available(n, false);
   vector<thread *> th(n, nullptr);
+
   for (int i = 0; i < m1[0].size(); i++)
     q.push(i);
   while (!q.empty()) {
@@ -65,26 +59,36 @@ void multConcurrency(const Matrix &m1, const Matrix &m2) {
       if (!available[i] and !q.empty()) {
         int nCol = q.front();
         q.pop();
-        th[i] = new thread(multCol, cref(m1), cref(m2), nCol, ref(result), i,
-                           ref(available));
+        th[i] = new thread(multCol, cref(m1), cref(m2), nCol, ref(result), i, ref(available));
       }
     }
   }
 
   for (int i = 0; i < th.size(); i++) {
-    if (th[i] != nullptr)
-      th[i]->join();
+    if (th[i] != nullptr) th[i]->join();
+    delete th[i];
   }
 
   cout << "Matrix:" << endl;
-  for (int i = 0; i < result.size(); i++) {
-    for (int j = 0; j < result[i].size(); j++) {
-      if (j)
-        cout << " ";
-      cout << result[i][j];
+  print(result);
+}
+
+void diamondCol(const Matrix &m1, const Matrix &m2, int nCol, Matrix &result,
+                int index, vector<bool> &available, int &cont) {
+
+  available[index] = true;
+  vector<int> col = getCol(m2, nCol);
+
+  for (int i = 0; i < m1.size(); i++) {
+    int mn = numeric_limits<int>::max();
+    for (int j = 0; j < m1[i].size(); j++) {
+      mn = min(mn, m1[i][j] + col[j]);
     }
-    cout << endl;
+    result[i][nCol] = mn;
   }
+
+  cont++;
+  available[index] = false;
 }
 
 void diamondConcurrency(const Matrix &m1, Matrix &m2) {
@@ -96,10 +100,11 @@ void diamondConcurrency(const Matrix &m1, Matrix &m2) {
 
   vector<bool> available(n, false);
   vector<thread *> th(n, nullptr);
-  for (int i = 0; i < m1[0].size(); i++) q.push(i);
 
   for (int t = 0; t < m1.size() - 1; t++) {
+    for (int i = 0; i < m1[0].size(); i++) q.push(i);
     result.assign(m1.size(), Vector(m1[0].size(), 0));
+
     while (true) {
       for (int i = 0; i < available.size(); i++) {
         if (!available[i] and !q.empty()) {
@@ -121,78 +126,31 @@ void diamondConcurrency(const Matrix &m1, Matrix &m2) {
     }
 
     m2 = result;
-    for (int i = 0; i < m1[0].size(); i++) q.push(i);
   }
 
   cout << "Matrix:" << endl;
-  for (int i = 0; i < result.size(); i++) {
-    for (int j = 0; j < result[i].size(); j++) {
-      if (j) cout << " ";
-      cout << result[i][j];
-    }
-    cout << endl;
-  }
+  print(result);
 }
 
 int main(int argc, char const *argv[]) {
   Matrix m1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-  Matrix m2 = {{2, 0, 1, 4}, {3, 0, 0, 0}, {5, 1, 1, 1}, {4, 5, 6, 6}};
-  Matrix m3 = {{2, 3, 4}, {5, 6, 7}, {8, 9, 10}};
+  Matrix m2 = {{2, 0, 1}, {3, 0, 0}, {5, 1, 1}};
+  Matrix m3 = {{1, 0, 1}, {1, 2, 1}, {1, 1, 0}};
 
-  // multConcurrency(m1, m1);
+  multConcurrency(m2, m3);
   // multConcurrency(m2, m2);
   Matrix mc = m1;
   diamondConcurrency(m1, mc);
   // diamondConcurrency(m1, m3);
 
-  // m1 X m2 = [[3 1 2]]
+  // m2 X m3 = [[3 1 2]]
   //            [3 0 3]
   //            [7 3 6]
   //
-  // m1 ^ m2 = [[1 2 1]]
-  //            [1 1 0]
-  //            [2 2 1]
-
-  // Multiplicacion
-  // Vector col1 = getCol(m1, 0);
-  // Vector col2 = getCol(m1, 1);
-  // Vector col3 = getCol(m1, 2);
-  //
-  // Matrix result(m1.size(), Vector(m1[0].size(), 0));
-  //
-  // thread t1(multCol, cref(m1), cref(m1), 0, ref(result));
-  // thread t2(multCol, cref(m1), cref(m1), 1, ref(result));
-  // thread t3(multCol, cref(m1), cref(m1), 2, ref(result));
-  //
-  // t1.join();
-  // t2.join();
-  // t3.join();
-  //
-  // cout << "Mult Matrix: " << endl;
-  // for (int i = 0; i < result.size(); i++) {
-  //   cout << result[i][0] << " " << result[i][1] << " " << result[i][2] <<
-  //   endl;
-  // }
-  // cout << endl;
-
-  // Operador diamante
-  // Vector dCol1(m1.size(), 0);
-  // Vector dCol2(m1.size(), 0);
-  // Vector dCol3(m1.size(), 0);
-  //
-  // thread t4(diamondCol, cref(m1), cref(col1), ref(dCol1));
-  // thread t5(diamondCol, cref(m1), cref(col2), ref(dCol2));
-  // thread t6(diamondCol, cref(m1), cref(col3), ref(dCol3));
-  //
-  // t4.join();
-  // t5.join();
-  // t6.join();
-  //
-  // cout << "Diamond Operator: " << endl;
-  // for (int i = 0; i < dCol1.size(); i++) {
-  //   cout << dCol1[i] << " " << dCol2[i] << " " << dCol3[i] << endl;
-  // }
-  // cout << endl;
+  // m1 <> m1 (n -1 veces) =
+  //            [[3 4 5]]
+  //             [6 7 8]
+  //             [9 10 11]
 
   return 0;
 }
